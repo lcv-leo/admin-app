110bcd47d2f9088f1928932c620af65281d68c2e
Leonardo Cardozo Vargas <268063598+lcv-leo@users.noreply.github.com>
2026-03-28 03:58:46 -0300

    chore: remove unused file and associated references

--- functions/api/mtasts/debug-env.ts
+++
@@ -1,4 +0,0 @@
-export async function onRequestGet(context: any) {
-  const keys = Object.keys(context.env)
-  return new Response(JSON.stringify(keys, null, 2), { headers: { 'Content-Type': 'application/json' } })
-}
